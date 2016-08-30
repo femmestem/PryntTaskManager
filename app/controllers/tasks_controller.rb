1d47425529e009bd3c9d1c16504a20c9ac760e2f
Christine L. Feaster <feasterc@gmail.com>
2016-08-29 20:25:01 -0700

    Add ability to create new tasks from UI

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,9 +8,17 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params.merge! creator: current_user)
+
+    if @task.save
+      redirect_to tasks_path, :notice => "Your task was successfully created."
+    else
+      redirect_to new_task_path, :alert => "Your task could not be saved. Make sure all fields are filled out and try again."
+    end
   end
 
   def edit
@@ -20,5 +28,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 end
